da208c6b498faaf6b2d79e76597f831bf26ced3b
Kingkimjh <kingjh3353@gmail.com>
2026-09-03 20:41:22 +0900

    Create code.c

---
+++ ch12/code.c
@@ -0,0 +1,22 @@
+// **********************************************
+// 제 목 : 변수 a, b, c의 주소를 출력하는 코드
+// 날 짜 : 2026년 9월 3일
+// 작성자 : 2600055 김준혁
+// **********************************************
+
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+int main(void)
+{
+char a = 'A';
+int b = 36;
+double c = 3.141592;
+
+printf("char형 변수 a의 주소: %u\n", &a);
+printf("int형 변수 b의 주소: %u\n", &b);
+printf("double형 변수 c의 주소: %u\n", &c);
+
+return 0;
+}
