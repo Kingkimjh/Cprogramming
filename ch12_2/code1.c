52b026337374efde0b029b56486559d698ea3f80
Kingkimjh <kingjh3353@gmail.com>
2026-09-08 21:42:28 +0900

    Update code1.c

--- ch12_2/code1.c
+++ ch12_2/code1.c
@@ -6,13 +6,13 @@ int main(void)
   char b = 'A';
   double c = 3.14;
 
-  int*pa = &a
-  char*pb = &b
-  double*pc = &c
+  int*pa = &a;
+  char*pb = &b;
+  double*pc = &c;
 
-  print("int형 변수 a의 값은: %d\n", *pa)
-  print("char형 변수 b의 값은: %c\n", *pb)
-  print("double형 변수 c의 값은: %lf\n", *pc)
+  print("int형 변수 a의 값은: %d\n", *pa);
+  print("char형 변수 b의 값은: %c\n", *pb);
+  print("double형 변수 c의 값은: %lf\n", *pc);
 
   return 0;
 }
