5eef0ce46efa54fc65e0f2c187b9f27ec5fd08f9
Kingkimjh <kingjh3353@gmail.com>
2026-09-12 00:27:25 +0900

    Create code1.c

---
+++ ch13-1/code1.c
@@ -0,0 +1,27 @@
+// **********************************************************
+//   제  목  :  배열 표현을 포인터 표현으로 변경하기
+//   날  짜  :  2026년 9월 10일
+//   작성자  :  2600055 김준혁
+// **********************************************************
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main() {
+	int grade[5];
+	int sum = 0, i, average;
+
+	for (i = 0; i < 5; i++) {
+		printf("성적을 입력하시오: ");
+		scanf("%d", (grade + i));
+		sum += *(grade + i);
+	}
+
+	average = sum / 5.0;
+	
+	printf("성적 평균= %d\n", average);
+	
+	return 0;
+}
