d32376f739ff016182053d19b42e731f1ad1656d
Kingkimjh <kingjh3353@gmail.com>
2026-09-17 15:56:26 +0900

    Update code1.c

--- ch13-2/code1.c
+++ ch13-2/code1.c
@@ -11,13 +11,7 @@ int main(void)
 
 	for (int i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] >= 'A' && c[i] <= 'Z')
-			c[i] = c[i] + 32;
-		else if (c[i] >= 'a' && c[i] <= 'z')
-			c[i] = c[i] - 32;
+		printf("%d번째문자 %c\n", i+1, c[i]);
 	}
-
-	printf("변환결과:%s", c);
-
 	return 0;
 }
